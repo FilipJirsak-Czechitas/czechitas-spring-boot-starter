79dc25ff9e5f960fea7220c815cc9a5cbbee1d16
Filip Jirsák <filip@jirsak.org>
2024-06-03 22:18:19 +0200

    Support for Pageable in Freemarker.

--- src/main/java/dev/czechitas/java/spring/freemarker/spring/FreemarkerConfig.java
+++ src/main/java/dev/czechitas/java/spring/freemarker/spring/FreemarkerConfig.java
@@ -37,7 +37,7 @@ public class FreemarkerConfig implements BeanPostProcessor {
   private static class Java17ObjectWrapperConfiguration extends DefaultObjectWrapperConfiguration {
     protected Java17ObjectWrapperConfiguration(Version incompatibleImprovements) {
       super(incompatibleImprovements);
-      setIterableSupport(true);
+//      setIterableSupport(true);
     }
   }
 }
